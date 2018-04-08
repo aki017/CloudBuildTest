b70f3570546ca8e4e0818142c22063e428d2d3f7
aki017 <aki@aki017.info>
2018-04-08 21:25:53 +0900

    init

---
+++ Assets/MainScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	  var json = Resources.Load<TextAsset>("UnityCloudBuildManifest.json");
+	  GetComponent<Text>().text = json == null ? "null" : json.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
